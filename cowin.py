081334a8ca6d6534eaa528baaa92a62763411d60
Srivatsa Rampalli <f2016290@pilani.bits-pilani.ac.in>
2021-06-06 14:45:26 +0530

    Add files via upload

--- cowin.py
+++ cowin.py
@@ -1,4 +1,5 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 import json
 import datetime
 
@@ -6,18 +7,21 @@ today = datetime.date.today().strftime('%d-%m-%Y')
 
 url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByCenter?center_id=603439&date="+str(today)
 
-response = urlopen(url)
+try:
+    response = urlopen(url)
+    data = json.loads(response.read())
+    available_dose_count = 0
+    total_available_dose_count = 0
 
-data = json.loads(response.read())
+    for session in data['centers']['sessions']:
+        total_available_dose_count+=(int(session['available_capacity']))
+        if(session['min_age_limit']==18):
+            available_dose_count+=int(session['available_capacity_dose1'])
 
-available_dose_count = 0
-total_available_dose_count = 0
+    if(available_dose_count>0):
+        print("18+ Available Doses in Apollo - "+ str(available_dose_count))
+        print("Total Available Doses in Apollo - "+ str(total_available_dose_count))
+except HTTPError as e:
+    content = e.read()
+    print(content)
 
-for session in data['centers']['sessions']:
-    total_available_dose_count+=(int(session['available_capacity']))
-    if(session['min_age_limit']==18):
-        available_dose_count+=int(session['available_capacity_dose1'])
-
-if(available_dose_count>0):
-    print("18+ Available Doses in Apollo - "+ str(available_dose_count))
-    print("Total Available Doses in Apollo - "+ str(total_available_dose_count))
